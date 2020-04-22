2cab2ad689adff74b298c0f02ce3c5536272fb67
Denis Arkhipov <denarxipow@yandex.ru>
2020-04-23 00:57:40 +0300

    сделал задание после освоения гита

---
+++ CSharpLevel_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Архипов Денис
+//Добавить свои объекты в иерархию объектов, чтобы получился красивый задний фон, похожий на полет в звездном пространстве.
+
+namespace CSharpLevel_2
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            
+            Form game_form   = new Form();
+            game_form.Width  = 800;
+            game_form.Height = 600;
+
+            game_form.Show();
+
+            Game.Initialize(game_form);
+            Game.Load();
+            Game.Draw();
+
+            Application.Run(game_form);
+        }
+    }
+}
---
+++ CSharpLevel_2/Star_2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CSharpLevel_2
+{
+    class Star_2 : VisualObject
+    {
+        public Star_2(Point Position, Point Direction, int Size) : base(Position, Direction, new Size(Size, Size))
+        {
+
+        }
+
+        public override void Draw(Graphics g)
+        {
+            Game.__Buffer.Graphics.FillRectangle(Brushes.White, _Position.X, _Position.Y, _Size.Width, _Size.Height);
+        }
+
+        public override void Update()
+        {
+            _Position.X = _Position.X + _Direction.X;
+            if (_Position.X < 0 - _Size.Width)
+            {
+                _Position.X = Game.Width + _Size.Width/2;
+            }
+
+        }
+    }
+}
