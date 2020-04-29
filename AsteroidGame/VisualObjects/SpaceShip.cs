fadb1a7cd91757986de58c3ada09e2d1c363574d
Denis Arkhipov <denarxipow@yandex.ru>
2020-04-29 23:37:25 +0300

    опред графич объектов Пуля и Корабль

---
+++ AsteroidGame/VisualObjects/SpaceShip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLevel_2.VisualObjects
+{
+    internal class SpaceShip : VisualObject
+    {
+        public SpaceShip(Point Position, Point Direction, Size Size, Image Image)
+            : base(Position, Direction, Size)
+        {
+
+        }
+        public override void Draw(Graphics g)
+        {
+
+        }
+    }
+}
