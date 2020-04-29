01db1751d3d34d5ebd78b6c6e78dfebd853a466e
Denis Arkhipov <denarxipow@yandex.ru>
2020-04-29 22:54:47 +0300

    Работа program.cs lesson-2

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -20,7 +20,25 @@ namespace Lesson_2
     {
          static void Main(string[] args)
         {
-          
+            Console.WriteLine("Вас приветствует программа вывода отсортированного массива сотрудников");
+
+            int numberOfWorkers = 20;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine(Environment.NewLine + "Создадим список сотрудников:");
+
+            ArrayOfWorkers arr = new ArrayOfWorkers();
+
+            arr.Init(numberOfWorkers);
+
+            arr.Print();
+
+            Console.WriteLine(Environment.NewLine + "Отсортируем список по зарплате:");
+
+            arr.Sort();
+            arr.Print();
+
+            Console.ReadKey();
         }
     }
 }
