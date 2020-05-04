25b98e11e60641c7198324dfe352f4218d36b53c
Denis Arkhipov <denarxipow@yandex.ru>
2020-05-04 13:28:05 +0300

    Задание с обобщенным делегатом

---
+++ Lesson-3/Observer2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+//Добавить в пример Lesson3 обобщенный делегат. 
+
+namespace Lesson_3
+{
+    partial class Program
+    {
+        class Observer2 
+        {
+            public void Do(object o)
+            {
+                Console.WriteLine($"Второй. Принял, что объект {0} побежал", o);
+            }
+        }
+    }
+}
---
+++ Lesson-3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Добавить в пример Lesson3 обобщенный делегат. 
+
+namespace Lesson_3
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            ObDelegate obBel = new ObDelegate();
+            Observer1 o1 = new Observer1();
+            Observer2 o2 = new Observer2();
+            Action<object> d1 = o1.Do;
+            obBel.Run += d1;
+            obBel.Run += o2.Do;
+            obBel.Start();
+            obBel.Run -= d1;
+            obBel.Start();
+        }
+    }
+}
